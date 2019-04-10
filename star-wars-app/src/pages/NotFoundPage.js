066ee835fe96519f71d299900c7a70cef8a51eb0
Majky336 <sojka.michal.96@gmail.com>
2019-04-10 21:15:43 +0200

    Starting point for frontend

--- star-wars-app/src/pages/NotFoundPage.js
+++ star-wars-app/src/pages/NotFoundPage.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 
-import "./NotFoundPage.css";
-
 class NotFoundPage extends Component {
   render() {
-    return <div className="not-found-container">I am not found page</div>;
+    return <div>I am not found page</div>;
   }
 }
 
