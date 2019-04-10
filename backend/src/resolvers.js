844f2472d90a0b592c5fdee99634b6a163910f6c
Robert Szlezak <robertszlezak@gmail.com>
2019-04-10 20:34:35 +0200

    Backend last commit

---
+++ backend/src/resolvers.js
@@ -0,0 +1,29 @@
+import episodeList from './mockData/episodeList';
+import characterList from './mockData/characterList';
+
+const resolvers = {
+  Query: {
+    helloWorld: () => `Hello from the dark side`,
+    getEpisodeList: () => episodeList,
+    getCharacterList: () => characterList,
+    getEpisode: (obj, { episodeIndex }) => episodeList[episodeIndex.value],
+    getCharacter: (obj, { characterIndex }) => characterList[characterIndex.value],
+  },
+  Mutation: {
+    createEpisode: (obj, { newEpisode }) => ({
+      id: `episode-${Date.now()}`,
+      ...newEpisode,
+    }),
+    createCharacter: (obj, { newCharacter }) => ({
+      id: `character-${Date.now()}`,
+      ...newCharacter,
+    }),
+  },
+  Episode: {
+    characters: obj => characterList.filter(item =>
+      obj.characters.find(characterId => characterId === item.id)
+    ),
+  },
+};
+
+export default resolvers;
