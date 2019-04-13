b2aa79ef3523428edfdc0a2f5178c56c1a731061
Majky336 <sojka.michal.96@gmail.com>
2019-04-13 07:36:53 +0200

    Added routes which will be used

--- star-wars-app/src/router/Router.js
+++ star-wars-app/src/router/Router.js
@@ -5,6 +5,8 @@ import Header from "../components/Header/Header";
 import CharacterDetailPage from "../pages/CharacterDetailPage/CharacterDetailPage";
 import EpisodeDetailPage from "../pages/EpisodeDetailPage/EpisodeDetailPage";
 import EpisodeListPage from "../pages/EpisodeListPage/EpisodeListPage";
+// import NewEpisodePage from "../pages/NewEpisodePage/NewEpisodePage";
+// import NewCharacterPage from "../pages/NewCharacterPage/NewCharacterPage";
 import NotFoundPage from "../pages/NotFoundPage";
 
 const AppRouter = () => (
@@ -13,8 +15,18 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route exact path="/" component={EpisodeListPage} />
-        <Route exact path="/episode-detail" component={EpisodeDetailPage} />
-        <Route exact path="/character-detail" component={CharacterDetailPage} />
+        <Route
+          exact
+          path="/episode-detail/:episodeIndex"
+          component={EpisodeDetailPage}
+        />
+        {/* <Route exact path="/new-episode" component={NewEpisodePage} /> */}
+        {/* <Route exact path="/new-character" component={NewCharacterPage} /> */}
+        <Route
+          exact
+          path="/character-detail/:characterIndex"
+          component={CharacterDetailPage}
+        />
         <Route path="*" component={NotFoundPage} />
       </Switch>
     </div>
